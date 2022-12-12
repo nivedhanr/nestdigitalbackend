89431eb571cf01103765b4ed4db20be68490d42a
nivedhanr <nivedharenahan@gmail.com>
2022-12-12 14:32:23 +0530

    emplogin

--- src/main/java/com/example/nestdigitalbackendspringboot/controller/AdminController.java
+++ src/main/java/com/example/nestdigitalbackendspringboot/controller/AdminController.java
@@ -36,11 +36,30 @@ public class AdminController {
     @PostMapping(path = "/delete",consumes = "application/json",produces = "application/json")
     public HashMap<String,String> Delete(@RequestBody Adminemployee d)
     {
-        String id=String.valueOf(d.getEmpcode());
+        String id=String.valueOf(d.getId());
         System.out.println(id);
-        dao.delete(d.getEmpcode());
+        dao.delete(d.getId());
         HashMap<String,String> map=new HashMap<>();
         map.put("status","success");
         return map;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userlogin", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> userLogin(@RequestBody Adminemployee r){
+        System.out.println(r.getEmail());
+        List<Adminemployee> result=(List<Adminemployee>) dao.userLogin(r.getEmail(),r.getPassword());
+        HashMap<String,String> st=new HashMap<>();
+        if (result.size()==0){
+            st.put("status","failed");
+            st.put("message","user doesn't exist");
+
+        }
+        else{
+            int id =result.get(0).getId();
+            st.put("userid",String.valueOf(id));
+            st.put("message","user login success");
+        }
+        return st;
     }
 }
